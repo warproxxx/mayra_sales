9755c7b01a3316a4adbad1d201692e8f7b590cfb
Daniel Sapkota <daniel@waterbot.xyz>
2020-08-25 13:36:28 +0545

    Fixed default country

--- src/project/resources/views/includes/countries.blade.php
+++ src/project/resources/views/includes/countries.blade.php
@@ -1,10 +1,5 @@
 <option value="">{{ $langg->lang157 }}</option>
-@if(Auth::check())
-	@foreach (DB::table('countries')->get() as $data)
-	<option value="{{ $data->country_name }}" {{ Auth::user()->country == $data->country_name ? 'selected' : '' }}>{{ $data->country_name }}</option>		
-	@endforeach
-@else
-	@foreach (DB::table('countries')->get() as $data)
-	<option value="{{ $data->country_name }}">{{ $data->country_name }}</option>		
-	@endforeach
-@endif
+
+@foreach (DB::table('countries')->get() as $data)
+	<option value="{{ $data->country_name }}" {{ $data->country_name == "Nepal" ? 'selected' : '' }}>{{ $data->country_name }}</option>		
+@endforeach
