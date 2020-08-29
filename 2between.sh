e89f6bcdd326e2785b1714fb7d54d3666211282d
Daniel Sapkota <daniel@waterbot.xyz>
2020-08-29 09:33:57 +0545

    Old is no longer required

--- 2between.sh
+++
@@ -1,8 +0,0 @@
-sudo apt-get update
-
-sudo apt-get install apache2 -y
-sudo apt-get install php -y
-sudo apt-get install php libapache2-mod-php php-mysql php-sqlite3 php-mbstring php-zip php-xml php-xmlrpc php-gd php-dom php-cli php-json php-common php-opcache php-readline php-curl -y
-
-sudo a2enmod rewrite
-sudo service apache2 restart
