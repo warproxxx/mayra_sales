72c8fe22a5db51c6a2ef94f84cb0c13f14d0e1f5
Daniel Sapkota <daniel@waterbot.xyz>
2020-09-24 12:09:47 +0545

    Make api token hidden

--- src/project/app/Models/User.php
+++ src/project/app/Models/User.php
@@ -11,7 +11,7 @@ class User extends Authenticatable
 
 
     protected $hidden = [
-        'password', 'remember_token'
+        'password', 'remember_token', 'api_token'
     ];
 
     public function IsVendor(){
