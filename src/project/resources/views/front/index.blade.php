5a7803eea6f62c41946f1849162d8ef504e85e2c
Daniel Sapkota <daniel@waterbot.xyz>
2020-09-24 13:00:10 +0545

    Premium

--- src/project/resources/views/front/index.blade.php
+++ src/project/resources/views/front/index.blade.php
@@ -86,32 +86,6 @@
 
 	@endif
 
-	<section class="categori-item electronics-section">
-			<div class="container">
-				<div class="row">
-					<div class="col-lg-12 remove-padding">
-						<div class="section-top">
-							<h2 class="section-title">
-							Premium
-							</h2>
-						</div>
-					</div>
-				</div>
-				<div class="row">
-					<div class="col-lg-12">
-						<div class="row">
-
-							@foreach($premium_products as $prod)
-								@include('includes.product.top-product')
-							@endforeach
-
-						</div>
-					</div>
-				</div>
-			</div>
-		</section>
-
-
 	@if($ps->featured == 1)
 		<!-- Trending Item Area Start -->
 		<section  class="trending">
@@ -140,6 +114,35 @@
 		</section>
 		<!-- Tranding Item Area End -->
 	@endif
+
+
+	<section class="categori-item electronics-section">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12 remove-padding">
+					<div class="section-top">
+						<h2 class="section-title">
+						Premium
+						</h2>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-lg-12">
+					<div class="row">
+
+						@foreach($premium_products as $prod)
+							@include('includes.product.top-product')
+						@endforeach
+
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+
+	
 
 	@if($ps->small_banner == 1)
 
