aa393a9db893023a1ff37069c7d6670c1597c21b
Daniel Sapkota <daniel@waterbot.xyz>
2020-10-31 09:58:13 +0545

    prolly done?

--- src/project/app/Http/Controllers/User/NotificationController.php
+++ src/project/app/Http/Controllers/User/NotificationController.php
@@ -65,4 +65,62 @@ class NotificationController extends Controller
             echo($e);
         }     
     } 
+
+    public function saveToken(Request $request)
+    {
+        Auth::user()->update(['device_token'=>$request->token]);
+        return response()->json(['token saved successfully.']);
+    }
+
+  
+
+    /**
+
+     * Write code on Method
+
+     *
+
+     * @return response()
+
+     */
+
+    public function sendNotification(Request $request)
+    {
+
+        $firebaseToken = User::whereNotNull('device_token')->pluck('device_token')->all();
+        $SERVER_API_KEY = 'AAAA1n09VQY:APA91bHnpqAyN8e7dwfMpsGYYIkTwWEb8mRatgRSgCmqU7vEowL1Kn4VztaF_ljwWBOKM5TeYL4q1MZ66Wkm-DvTHjF43y9paU0nEIcicRqcCX6NN8O8FtHb8cKGD__y4NxUhj1hSj_m';
+
+        $data = [
+
+            "registration_ids" => $firebaseToken,
+            "notification" => [
+                "title" => $request->title,
+                "body" => $request->body,  
+            ]
+        ];
+
+        $dataString = json_encode($data);
+    
+        $headers = [
+            'Authorization: key=' . $SERVER_API_KEY,
+            'Content-Type: application/json',
+        ];
+
+
+        $ch = curl_init();
+      
+
+        curl_setopt($ch, CURLOPT_URL, 'https://fcm.googleapis.com/fcm/send');
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $dataString);
+
+        $response = curl_exec($ch);
+
+        dd($response);
+
+    }
+
 }
